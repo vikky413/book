aae982d9c87d3c699d5f3d6c0998e5c71e281b91
Vikram Singh <vikramsinghrewa2001@gmail.com>
2023-03-15 02:20:54 +0530

    add atlas

--- src/db/connect.js
+++ src/db/connect.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 mongoose.set('strictQuery', false);
 
-mongoose.connect("mongodb://127.0.0.1:27017/RegistrationData",).then(() => {
+mongoose.connect("mongodb+srv://bhavanaeligeti5:Bhavana123@cluster0.vx1hapl.mongodb.net/?retryWrites=true&w=majority",).then(() => {
     console.log(`connection successful`);
 }).catch((e) => {
     console.log(`no connection`);
